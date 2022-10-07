feed97e1e12024c48b20af46bdcbcf8b74d88d0b
Rafael Gagliardi Maciel da Cruz <rcruz4513@yahoo.com.br>
2022-10-07 11:10:30 -0300

    Initial commit

---
+++ Aula13/ex15/script.js
@@ -0,0 +1,50 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('Erro! Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                // criança
+                img.setAttribute('src', 'bebehomem.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'jovemhomem.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'adultohomem.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idosohomem.png')
+            }         
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'bebemulher.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovemmulher.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'adultomulher.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idosamulher.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.` 
+        res.appendChild(img)       
+    }
+}
